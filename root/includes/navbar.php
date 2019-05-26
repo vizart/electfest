bad71bed3550c3cf0cea89ec70dd797075c6470d
plamenlakov <frainbreeze3@gmail.com>
2019-05-26 20:35:49 +0200

    Updated navbar

--- root/includes/navbar.php
+++ root/includes/navbar.php
@@ -1,11 +1,11 @@
 <div id="navigation">
         <nav>
             <div class="nav-logo">
-                <h4><a href="index.php">ELECTFEST</a></h4>
+                <img src="../source/logo.png" alt="">
             </div>
             <ul class="nav-links">
                 <li><a href="Article_page.php">Buy Tickets</a></li>
-                <li><a href="Video_page.php">About Us</a></li>
+                <li><img src="../source/logo.png" alt=""></li>
                 <li><a href="Video_page.php">Log In</a></li>
             </ul>
             <div class="burger">
